2a68b67079c4de24f0d43ba1364554ef917b8bcf
Raiyan.S <rayan.subedarLive@gmail.com>
2025-02-25 10:49:54 +0300

    allow_origins update

--- Back-End/main.py
+++ Back-End/main.py
@@ -18,7 +18,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5000"], # Allows requests from the frontend
+    allow_origins=["http://localhost:8000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
