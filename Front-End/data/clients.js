8013fac1a98ec6ffc340dd9d8051f25ed8514b6e
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2025-03-06 01:09:51 +0300

    Update clients.js

--- Front-End/data/clients.js
+++ Front-End/data/clients.js
@@ -12,6 +12,7 @@ export const clients = [];
 export const loadClients = async () => {
   try {
     const clientIds = await fetchUniqueClientIds();
+    clientIds.sort(); // Sort the client IDs incrementally
     clientIds.forEach((id, index) => {
       clients.push({ id, name: `Client ${index + 1}`});
     });
