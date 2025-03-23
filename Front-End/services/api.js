f777142c0b94718d875f3999752029bdbd464a13
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2025-03-23 04:32:56 +0300

    credentials include

--- Front-End/services/api.js
+++ Front-End/services/api.js
@@ -51,6 +51,7 @@ export const login = async (email, password) => {
     headers: {
       'Content-Type': 'application/json',
     },
+    credentials: 'include',
     body: JSON.stringify({ email, password }),
   });
   if (!response.ok) {
@@ -65,6 +66,7 @@ export const register = async (email, password) => {
     headers: {
       'Content-Type': 'application/json',
     },
+    credentials: 'include',
     body: JSON.stringify({ email, password }),
   });
   if (!response.ok) {
