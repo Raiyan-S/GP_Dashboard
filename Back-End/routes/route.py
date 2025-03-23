ea0713684abfc39a01ccab721ed28472c4e44818
Raiyan.S <85458945+Raiyan-S@users.noreply.github.com>
2025-03-23 21:42:44 +0300

    Update route.py

--- Back-End/routes/route.py
+++ Back-End/routes/route.py
@@ -36,7 +36,7 @@ async def get_unique_client_ids():
             {"$group": {"_id": "$clients.client_id"}},
             {"$sort": {"_id": 1}}  # Sort the client IDs in ascending order
         ]
-        result = await db['test'].aggregate(pipeline).to_list(1000)
+        result = await db['training_rounds'].aggregate(pipeline).to_list(1000)
         client_ids = [doc["_id"] for doc in result]
         return client_ids
     except Exception as e:
@@ -59,7 +59,7 @@ async def get_client_rounds(client_id: str):
                 }
             }
         ]
-        rounds = await db['test'].aggregate(pipeline).to_list(1000)
+        rounds = await db['training_rounds'].aggregate(pipeline).to_list(1000)
         # Map the data to the required format, simplifying the structure
         simplified_rounds = [
             {
